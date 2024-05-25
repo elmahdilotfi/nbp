716cc8d94b6cba889ece4cc8ede1d8adede08ad8
LOTFI El Mahdi <lotfielmahdi101@gmail.com>
2024-05-25 13:54:04 -0500

    Add project metadata.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+__title__ = "nbp"
+__description__ = "Network devices configuration backup"
+__author__ = "LOTFI El Mahdi"
+__author_email__ = "lotfielmahdi101@gmail.com"
+__version__ = "0.0.0"
+__copyright__ = "Copyright LOTFI El Mahdi"
+
